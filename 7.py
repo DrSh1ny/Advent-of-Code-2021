424300a6c76b12c9ef49feb4953cee81b697a957
Francisco Fernandes <francisco201262@gmail.com>
2022-03-11 12:03:09 +0000

    day 7

--- 7.py
+++ 7.py
@@ -1,3 +1,6 @@
+from statistics import mean, median
+
+
 def readInput(targetFile):
     file=open(targetFile,"r")
     inp=file.readlines()
@@ -6,7 +9,35 @@ def readInput(targetFile):
     return inp
     
 def computeAnswerPart1(input):
-    return None
+    centroid=round(median(input))
+    fuel=0
+    for crab in input:
+        fuel+=abs(crab-centroid)
+    return centroid,fuel
+
+def computeCostFuel(input,centroid):
+    fuel=0
+    for crab in input:
+        dist=abs(crab-centroid)
+        cost=round(dist*(dist+1)/2)
+        fuel+=cost
+    return fuel
+
+def computeAnswerPart2(input):
+    lastCentroid=None
+    costLastCentroid=None
+    for centroid in range(min(input),max(input)+1):
+        cost=computeCostFuel(input,centroid)
+        if costLastCentroid==None or  costLastCentroid>=cost:
+            lastCentroid=centroid
+            costLastCentroid=cost
+    return lastCentroid,costLastCentroid
+
 
 if __name__=="__main__":
-    pass
+    input=readInput("7.txt")
+    print(input)
+    ans=computeAnswerPart1(input)
+    print(ans)
+    ans=computeAnswerPart2(input)
+    print(ans)
